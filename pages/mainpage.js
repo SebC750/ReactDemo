fea1db42929d2b7dc0be8b8c4ae0cd442ccb58a5
SebC750 <SebC89999@gmail.com>
2023-10-09 21:14:14 -0400

    git commit

---
+++ pages/mainpage.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const MainMenu = () => {
+    return (
+        <div>
+          <h1> Batchan </h1>
+          <p> Welcome to batchan! Talk about anything goth related! </p>
+
+          <h3> Portals </h3>
+          <ul>
+            <li> 
+               <p> General <button> Enter </button> </p>
+            </li>
+
+            <li> 
+            <p> Music <button> Enter </button> </p>
+            </li>
+            
+            <li> 
+            <p> Fashion <button> Enter </button> </p>
+            </li>
+
+            <li> 
+            <p> Philosophy <button> Enter </button> </p>
+            </li>
+
+            <li> 
+            <p> <button> Enter </button> </p>
+            </li>
+          </ul>
+        </div>
+    );
+}
